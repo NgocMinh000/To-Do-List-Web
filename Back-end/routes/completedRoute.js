c345b1ef4f84887d348fb98b60ba38ccb1b927b0
pxphbn8 <160334114+pxphbn8@users.noreply.github.com>
2025-01-01 00:00:43 +0700

    Delete Back-end directory

--- Back-end/routes/completedRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getCompleted } from '../controllers/completedController.js';
-
-const completedRoutes = express.Router();
-
-completedRoutes.get('/getCompleted', getCompleted); // Xác định route GET /completed để gọi hàm getCompleted
-
-export default completedRoutes;
